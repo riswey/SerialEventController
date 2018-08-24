ba6351b12cb711c6bd910f3ef66ce392258418ea
Alva Gosson <riswey@hotmail.com>
2018-08-24 14:25:05 +0100

    Working v1.0

---
+++ SerialEventController/SerialEventController.h
@@ -0,0 +1,70 @@
+#pragma once
+
+/*
+* Packets look like this:
+* SHIFT_OUT + SHIFT_OUT + COMMAND_CHR [+ body string] + NEW_LINE
+*/
+
+typedef void(*serialeventhandler_t)(int chr, String data);
+
+#define BUFFERLIMIT 200
+#define MAXEVENTS 10
+
+class SerialEventController {
+	//just hardcoded for now
+	String SIGNATURE = "\x0E\x0E";    //Shift Out
+	const char terminal = '\n';             //\n
+	String buf = "";  //remember: volatile if changed by interrupt!
+
+	serialeventhandler_t eventfuncs[MAXEVENTS];
+	String eventindex = "";   //chr at position maps to eventfuncs[]
+
+	int HandleLine(const String line) { //must be pre-trimmed!
+		if (line.length() < 3) return 1;  //header failed
+		if (line.substring(0, 2) != SIGNATURE) return 1;  //Signature failed
+		char chr = line[2];
+		String data = line.substring(3);
+
+		int idx = eventindex.indexOf(chr);
+
+		eventfuncs[idx](chr, data);
+		return 0;
+	}
+
+public:
+	SerialEventController() {
+		buf.reserve(BUFFERLIMIT);
+	}
+
+	int RegisterEventHandler(const char c, const serialeventhandler_t seh) {
+		eventindex += c;
+		if (eventindex.length() > MAXEVENTS) return 1;
+		eventfuncs[eventindex.length() - 1] = seh;
+		return 0;
+	}
+
+	bool AddCharacter(const char c) {
+		if (c == terminal) {
+			HandleLine(buf);
+			buf = "";
+		}
+		else {
+			buf += c;
+		}
+	}
+
+	void SendData(const char c, const String data) {
+		Serial.println(SIGNATURE + c + data);
+	}
+
+	void SendData(const char c, const int data) {
+		Serial.println(SIGNATURE + c + String(data));
+	}
+
+	void SendData(const char c, const float data) {
+		Serial.println(SIGNATURE + c + String(data));
+	}
+
+};
+
+SerialEventController SEController;
--- SerialEventController/SerialEventController.ino
+++ SerialEventController/SerialEventController.ino
@@ -4,12 +4,75 @@
  Author:	Alva
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#include "SerialEventController.h"
+
+enum CMD {
+	DESIREDSPEED = 34,
+	START,
+	STOP,
+	REQUESTSPEED
+};
+
+//example Arduino state that can be set and interrogated
+int pidspeed = 100;
+
+void serialEvent() {
+	while (Serial.available()) {
+		char in_char = (char)Serial.read();
+		//;;;;; NOTE
+		//Add serial input to SerialEventController
+		//Can do in loop() also
+		SEController.AddCharacter(in_char);
+	}
+}
+
+//;;;;; SEController calls these functions when registered character appears in serial line
+
+//
+static void setPIDSpeed(int cmd, String data) {
+	pidspeed = data.toInt();
+	Serial.println(String(pidspeed)); //Can send on serial output as usual. Client looks for: SHIFT_OUT + SHIFT_OUT... starting packed signature");
+}
+
+static void someOtherEvents(int cmd, String data) {
+	switch (cmd) {
+	case START:
+		//do something
+		break;
+	case STOP:
+		//do something
+		break;
+	}
+}
+
+static void requestPIDSpeed(int cmd, String data) {
+	switch (cmd) {
+	case REQUESTSPEED:
+		//NOTE: echo the cmd code
+		SEController.SendData(cmd, pidspeed);
+		break;
+	}
+}
+
+void setup()
+{
+	//;;;;; NOTE
+	//Single function pattern
+	SEController.RegisterEventHandler(DESIREDSPEED, setPIDSpeed);
+	//Switch pattern
+	SEController.RegisterEventHandler(START, someOtherEvents);
+	SEController.RegisterEventHandler(STOP, someOtherEvents);
+	//Request data
+	SEController.RegisterEventHandler(REQUESTSPEED, requestPIDSpeed);
+
+	Serial.begin(9600);
+
+	//auto update client
+	SEController.SendData(REQUESTSPEED, pidspeed);
 
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+
 }
