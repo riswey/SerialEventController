f2863d487596be922fdc4cb0879d5a197001ef1e
Alva Gosson <riswey@hotmail.com>
2018-08-24 14:20:41 +0100

    Add project files.

---
+++ SerialEventController/SerialEventController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SerialEventController.ino
+ Created:	8/24/2018 2:20:33 PM
+ Author:	Alva
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
